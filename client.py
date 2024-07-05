3799d13f041f11b09f50674c4a7b3e9c181e5cfe
Kashish jain <165054560+kashijain@users.noreply.github.com>
2024-07-05 22:58:43 +0530

    Add files via upload

---
+++ client.py
@@ -0,0 +1,16 @@
+# from openai import OpenAI
+
+# client = OpenAI(
+#   api_key ="sk-proj-zH2GSjf6GS27yO8leEdhT3BlbkFJigFFQ5vZTFwr3QgRNJ6P",
+# )
+
+
+# completion = client.chat.completions.create(
+#   model="gpt-3.5-turbo",
+#   messages=[
+#     {"role": "system", "content": "You are a virtual  assistant named jarvis, skilled in  genral taks like alexa and google cloud"},
+#     {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+#   ]
+# )
+
+# print(completion.choices[0].message.content)
